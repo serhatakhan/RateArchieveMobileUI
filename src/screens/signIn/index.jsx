f5c6bba3cc1953311251f724645caf8bd03a97e0
SaruGit75 <srhnfrkn75@gmail.com>
2024-06-01 18:34:50 +0300

    ui kitten installed

--- src/screens/signIn/index.jsx
+++ src/screens/signIn/index.jsx
@@ -1,27 +1,43 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import {
   View,
-  Text,
   StyleSheet,
-  Image,
-  SafeAreaView,
-  ImageBackground,
-  TouchableOpacity,
+  Image
 } from 'react-native';
 import { width } from '../../utils/constants';
+import { Input } from '@ui-kitten/components';
 
-const SignIn = ({navigation}) => {
+const SignIn = ({ navigation }) => {
+  
+  const useInputState = (initialValue = '') => {
+    const [value, setValue] = React.useState(initialValue);
+    return { value, onChangeText: setValue };
+  };
+
+  const mediumInputState = useInputState();
+
   return (
-    <View style={{flex: 1}}>
+    <View style={{ flex: 1 }}>
       <View style={styles.imageWrapper}>
         <Image
           source={require('../../assets/ciro1.png')}
-          style={{resizeMode: 'contain', flex: 1, width: width-90}}
+          style={{ resizeMode: 'contain', flex: 1, width: width - 90 }}
         />
       </View>
 
-      <View style={{flex: 1.25}}>
-        {/* <Image source={require('../../assets/ciro.jpg')} /> */}
+      <View style={styles.inputArea}>
+        <Input
+          size='medium'
+          placeholder='Medium'
+          {...mediumInputState}
+          label={'User Name'}
+        />
+        <Input
+          size='medium'
+          placeholder='Medium'
+          {...mediumInputState}
+          label={'Password'}
+        />
       </View>
     </View>
   );
@@ -50,6 +66,12 @@ const styles = StyleSheet.create({
   },
   buttonWrapper: {
     flex: 1,
+  },
+  inputArea: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+
   },
 });
 
